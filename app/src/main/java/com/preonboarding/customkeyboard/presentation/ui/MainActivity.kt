9ce7b958383a2bb03f62b480abbd79eb53ab2904
Hyunkuk <boris0815@naver.com>
2022-10-13 01:05:59 +0900

    feat : ComponentActivity 로 변경

---
+++ app/src/main/java/com/preonboarding/customkeyboard/presentation/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.preonboarding.customkeyboard.presentation.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CustomKeyBoardApp()
+        }
+    }
+}
